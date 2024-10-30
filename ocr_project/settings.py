001226c12293d175b10d941646122ef7e84e3740
syed <130492128+hlmgohar@users.noreply.github.com>
2024-10-30 11:29:02 +0500

    changes

--- ocr_project/settings.py
+++ ocr_project/settings.py
@@ -57,6 +57,8 @@ CORS_ALLOW_METHODS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
