a0f88be16d9eeeee100ecb5c2e4d53de32a506b2
syed <130492128+hlmgohar@users.noreply.github.com>
2024-11-05 13:10:32 +0500

    Update settings.py

--- ocr_project/settings.py
+++ ocr_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 'https://antariksh-frontend-2l3thwfm3-hlmgohars-projects.vercel.app'
+    'http://localhost:3000', 'https://antariksh-frontend-p6gn18w41-hlmgohars-projects.vercel.app'
 ]
 
 CORS_ALLOW_HEADERS = [
