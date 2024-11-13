4b0c33ce4e9a1bc54b3c25512a86b92a0e67f4da
syed <130492128+hlmgohar@users.noreply.github.com>
2024-11-13 11:38:19 +0500

    new changes

--- ocr_project/deployment.py
+++ ocr_project/deployment.py
@@ -8,13 +8,14 @@ ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
 
-# WhiteNoise configuration
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Should be placed at the top
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-] 
+]
